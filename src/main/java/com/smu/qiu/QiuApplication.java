4a0b96b086f4bae2c39af3a2e16c259c3fcd14d9
jia <shenjiazhiyuan@163.com>
2021-03-19 14:12:39 +0800

    Initial commit

---
+++ src/main/java/com/smu/qiu/QiuApplication.java
@@ -0,0 +1,13 @@
+package com.smu.qiu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QiuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QiuApplication.class, args);
+	}
+
+}
